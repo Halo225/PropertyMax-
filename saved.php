aac04677a88b8444509ff079679dd449a5a02985
Halo225 <122497812+Halo225@users.noreply.github.com>
2023-09-15 11:53:45 +0100

    Add files via upload

---
+++ saved.php
@@ -0,0 +1,46 @@
+<?php  
+
+include 'components/connect.php';
+
+if(isset($_COOKIE['user_id'])){
+   $user_id = $_COOKIE['user_id'];
+}else{
+   $user_id = '';
+}
+
+include 'components/save_send.php';
+
+?>
+
+<?php include 'components/user_header.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/soon.css">
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+</body>
+</html>
+
+<div class="style">
+ <div class="squ">
+
+   <h1>
+    FEATURES COMING SOON
+   
+   </h1>
+  
+</div>
+</div>
+<?php include 'components/footer.php'; ?>
