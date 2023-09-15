bf3c22ecdf4d056511cbb4a59c75b785ea23fdda
Halo225 <122497812+Halo225@users.noreply.github.com>
2023-09-15 11:49:37 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,35 @@
+<!-- footer section starts  -->
+
+<footer class="footer">
+
+   <section class="flex">
+
+      <div class="box">
+         <a href="tel:08118530089"><i class="fas fa-phone"></i><span>08118530089</span></a>
+         <a href="tel:08164307533"><i class="fas fa-phone"></i><span>08164307533</span></a>
+         <a href="emmanuelnwaju5@gmail.com"><i class="fas fa-envelope"></i><span>emmanuelnwaju5@gmail.com</span></a>
+      </div>
+
+      <div class="box">
+         <a href="Home.php"><span>Home</span></a>
+         <a href="About.php"><span>About</span></a>
+         <a href="Contact.php"><span>Contact</span></a>
+         <a href="Listings.php"><span>All Listings</span></a>
+         <a href="Saved.php"><span>Saved Properties</span></a>
+      </div>
+
+      <div class="box">
+         <a href="#"><span>Facebook</span><i class="fab fa-facebook-f"></i></a>
+         <a href="#"><span>Twitter</span><i class="fab fa-twitter"></i></a>
+         <a href="#"><span>Linkedin</span><i class="fab fa-linkedin"></i></a>
+         <a href="#"><span>Instagram</span><i class="fab fa-instagram"></i></a>
+
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; Copyright @ <?= date('Y'); ?> by <span>CODE MASTER</span> | All Rights Reserved!</div>
+
+</footer>
+
+<!-- footer section ends -->
